01f057a73e49c322e33e8f5184293115b6b305c6
dpurser-dev <david.purser@kellogg.northwestern.edu>
2022-04-29 22:12:46 +0000

    v1

--- kmdb.rb
+++ kmdb.rb
@@ -69,10 +69,18 @@
 
 # Delete existing data, so you'll start fresh each time this script is run.
 # Use `Model.destroy_all` code.
-# TODO!
+
+Model.destroy_all
 
 # Generate models and tables, according to the domain model.
-# TODO!
+
+# Run in the terminal
+# Rails generate model Studio
+# Rails generate model Movie
+# Rails generate model Actor
+# Rails generate model Role
+
+
 
 # Insert data into the database that reflects the sample data shown above.
 # Do not use hard-coded foreign key IDs.
